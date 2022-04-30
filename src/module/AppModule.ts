2e0be53654d0b3cba1c796a5c22f6137a6042979
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2022-04-30 18:05:03 +0900

    update dependencies
    - Add @nestjs/apollo package for latest @nestjs/graphql

--- src/module/AppModule.ts
+++ src/module/AppModule.ts
@@ -1,16 +1,17 @@
 import { resolve } from "path";
 
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 import { HelloController } from "../controller/HelloController";
-
 import { QueryResolver } from "../resolver/QueryResolver";
 
 @Module({
     imports: [
-        GraphQLModule.forRoot({
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
             typePaths: [
                 //
                 resolve(__dirname, "../../schema.graphql"),
